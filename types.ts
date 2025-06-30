cdded2e146e9514ebf7d28c4104ea3997888af02
Mattia Natale <mattianatale07@gmail.com>
2025-06-30 19:49:21 +0200

    feat: add videogame.json & types.ts

---
+++ types.ts
@@ -0,0 +1,10 @@
+export type Videogame = {
+  title: string;
+  category: string;
+  platform: string;
+  developer: string;
+  releaseYear: number;
+  rating: number;
+  multiplayer: boolean;
+  price: number;
+};
